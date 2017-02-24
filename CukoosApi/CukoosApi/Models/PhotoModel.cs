9606755b9d77d19ee655e178a682f000893ae935
Eliran Pe'er <eliran013@gmail.com>
2017-02-24 13:50:04 +0000

    Adds models to the server

---
+++ CukoosApi/CukoosApi/Models/PhotoModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CukoosApi.Models
+{
+    public class PhotoModel
+    {
+        #region Properties
+        public int id { get; set; }
+        public string title { get; set; }
+        public byte[] image { get; set; }
+        public CategoryModel Category { get; set; }
+        #endregion
+
+        #region Consturctors
+        public PhotoModel()
+        {
+        }
+
+        public PhotoModel(Photo entity)
+        {
+            this.id = entity.id;
+            this.title = entity.title;
+            this.image = entity.image;
+        }
+
+        public Photo ToEntity()
+        {
+            return new Photo()
+            {
+                id = this.id,
+                title = this.title,
+                image = this.image,
+                category = this.Category.id,
+            };
+        }
+        #endregion
+    }
+}
