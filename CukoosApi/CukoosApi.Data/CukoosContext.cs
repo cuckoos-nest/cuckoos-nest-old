50b5dbfbab87f0dfd16a5efaf741806cdcd4427f
shamtest@mailinator.com <shamtest@mailinator.com@707200b2-fa8c-11e6-aefd-5de80ca18fd9>
2017-02-24 18:14:25 +0000

    Server: 1. Start creating DbContext for Entity Framework 2. Start building repositories to work with the DbContext

---
+++ CukoosApi/CukoosApi.Data/CukoosContext.cs
@@ -0,0 +1,22 @@
+﻿using CukoosApi.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CukoosApi.Data
+{
+  public class CukoosContext : DbContext
+  {
+    public CukoosContext()
+      : base("name=CukoosContext")
+    {
+
+    }
+    public virtual DbSet<Category> Categories { get; set; }
+    public virtual DbSet<Photo> Photos { get; set; }
+
+  }
+}
---
+++ CukoosApi/CukoosApi.Repository/Interfaces/IPhotoRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CukoosApi.Repository.Interfaces
+{
+  /// <summary>
+  /// All special photos methods go here
+  /// </summary>
+  public interface IPhotoRepository
+  {
+  }
+}
