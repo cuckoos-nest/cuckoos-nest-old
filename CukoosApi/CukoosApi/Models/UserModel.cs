51bb8ebcbaf3b9122eb57d5e13827b4cc00d56cb
Eliran Pe'er <eliran013@gmail.com>
2017-02-24 14:47:50 +0000

    UserUploadModel and UserModel added

---
+++ CukoosApi/CukoosApi/Models/UserModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CukoosApi.Models
+{
+    public class UserModel
+    {
+        #region Properties
+        public int id { get; set; }
+        public int fb_id { get; set; }
+        public IEnumerable<PhotoModel> followingPhotos { get; set; }
+        public IEnumerable<CategoryModel> followingCategories { get; set; }
+        public IEnumerable<UserUploadModel> uploads { get; set; }
+        #endregion
+
+        #region Consturctors
+        public UserModel()
+        {
+        }
+
+        public UserModel(User entity)
+        {
+            this.id = entity.id;
+            this.fb_id = entity.fb_id;
+            this.followingPhotos = entity.Photos.ToList().Select(x => new PhotoModel(x));
+            this.followingCategories = entity.Categories.ToList().Select(x => new CategoryModel(x));
+            this.uploads = entity.UserUploads.ToList().Select(x => new UserUploadModel(x));
+        }
+
+        public User ToEntity()
+        {
+            return new User()
+            {
+                id = this.id,
+                fb_id = this.fb_id,
+                Photos = this.followingPhotos.Select(x => x.ToEntity()) as ICollection<Photo>,
+                Categories = this.followingCategories.Select(x => x.ToEntity()) as ICollection<Category>,
+                UserUploads = this.uploads.Select(x => x.ToEntity()) as ICollection<UserUpload>,
+            };
+        }
+        #endregion
+    }
+}
---
+++ CukoosApi/CukoosApi/Models/UserUploadModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CukoosApi.Models
+{
+    public class UserUploadModel
+    {
+        #region Properties
+        public int id { get; set; }
+        public PhotoModel photo { get; set; }
+        public byte[] userPhoto { get; set; }
+        public string description { get; set; }
+        public UserModel user { get; set; }
+        #endregion
+
+        #region Consturctors
+        public UserUploadModel()
+        {
+        }
+
+        public UserUploadModel(UserUpload entity)
+        {
+            this.id = entity.id;
+            this.photo = new PhotoModel(entity.Photo1);
+            this.userPhoto = entity.user_photo;
+            this.description = entity.description;
+            this.user = new UserModel(entity.User1);
+        }
+
+        public UserUpload ToEntity()
+        {
+            return new UserUpload()
+            {
+                id = this.id,
+                photo = this.photo.id,
+                user_photo = this.userPhoto,
+                description = this.description,
+                user = this.user.id,
+            };
+        }
+        #endregion
+    }
+}
