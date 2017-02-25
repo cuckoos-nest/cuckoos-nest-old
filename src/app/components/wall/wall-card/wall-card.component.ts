0fae995c422f05628a617572b6f308af7cc7bdf6
eliran013@gmail.com <eliran013@gmail.com@707200b2-fa8c-11e6-aefd-5de80ca18fd9>
2017-02-25 13:37:51 +0000

    Wall card now uses the api

--- src/app/components/wall/wall-card/wall-card.component.ts
+++ src/app/components/wall/wall-card/wall-card.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { UserUploadModel } from '../../../models/user-upload.model';
 
 @Component({
-    selector: '<wall-card></wall-card>',
+    selector: 'wall-card',
     templateUrl: 'wall-card.html'
 })
 export class WallCardComponent {
 
+    @Input("data")
+    public userUpload: UserUploadModel;
+
     constructor() {
-        
+    }
+
+    private stringifyTime(dateTime: string): string {
+        return "";
     }    
 }
