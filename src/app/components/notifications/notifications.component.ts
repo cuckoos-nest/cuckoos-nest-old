a12fa2ce145abe83959041eabc6f5b026b1b14c1
Eliran Pe'er <eliran013@gmail.com>
2017-02-26 20:38:35 +0000

    #9 - notifications.component has been created, and linked from the main menu

---
+++ src/app/components/notifications/notifications.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'notifications',
+    templateUrl: 'notifications.html'
+})
+export class NotificationsComponent {
+
+
+
+    constructor() {
+
+    }    
+}
