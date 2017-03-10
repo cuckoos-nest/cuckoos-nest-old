ff00f65b618d970cd28e56cf7fb486cbd52e590b
shamtest@mailinator.com <shamtest@mailinator.com@707200b2-fa8c-11e6-aefd-5de80ca18fd9>
2017-03-10 18:04:31 +0000

    photo detail page

--- src/app/services/user-upload.service.ts
+++ src/app/services/user-upload.service.ts
@@ -6,6 +6,7 @@ import 'rxjs/add/operator/map';
 import { BaseService } from './base/base.service';
 
 import { UserUploadModel } from '../models/user-upload.model';
+import { PhotoModel } from '../models/photo.model';
 
 import Config from '../config.json';
 
@@ -14,5 +15,14 @@ export class UserUploadService extends BaseService {
 
     constructor(private http: Http) {
         super();
+    }
+
+    getMostPopularPhotosByPhotoId(id : number, from?: number, take?: number) : Observable<UserUploadModel[]> {
+        if (from && from != 0 && take && take != 0)
+            return this.http.get(`${this.userUploadDirectory}/popular/photos/${id}/${from}/${take}`).map(userUploads => userUploads.json());
+        else if (from && from != 0)
+            return this.http.get(`${this.userUploadDirectory}/popular/photos/${id}/${from}`).map(userUploads => userUploads.json());
+        else
+            return this.http.get(`${this.userUploadDirectory}/popular/photos/${id}`).map(userUploads => userUploads.json());
     }
 }
