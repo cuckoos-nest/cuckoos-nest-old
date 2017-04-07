f03ed86ca9baf3a948293c6abb15b4214b405068
shamtest <16955002+jackiegit@users.noreply.github.com>
2017-04-07 11:52:51 +0000

    renaming and redefining user schema model

--- src/app/models/category.model.ts
+++ src/app/models/category.model.ts
@@ -3,6 +3,6 @@ import { BaseModel } from './base/base.model';
 export class CategoryModel extends BaseModel {
      name : string;
      image: string;
-     numberOfUploads: number;
-     numberOfFollowers: number;
+     uploadsCount: number;
+     followersCount: number;
 }
